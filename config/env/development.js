910c8af4c06ae89eb53192d4d0e6fc6403d08886
Chae Lee <arfarf89@gmail.com>
2014-03-29 17:57:25 +0900

    db setting

--- config/env/development.js
+++ config/env/development.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    db: 'mongodb://localhost/mean-dev',
+    db: 'mongodb://ds035517.mongolab.com:35517/heroku_app23507748/mean-dev',
     app: {
         name: 'MEAN - A Modern Stack - Development'
     },
